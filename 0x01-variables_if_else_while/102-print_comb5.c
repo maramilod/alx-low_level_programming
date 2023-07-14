b22b0016499b69dcb88757a54639a821361c739d
maramilod <marammilod03@gmail.com>
2023-07-14 04:28:58 -0700

    eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * putchar: output
+ * Return: Always (0)
+ */
+int main(void)
+{
+int a, b, c, d;
+for (a = 0; a <= 9; a++)
+for (b = 0; b < 9; b++)
+for (c = 0; c <= 9; c++)
+for (d = b+1; d <= 9; d++)
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(' ');
+putchar(c + '0');
+putchar(d + '0');
+if ( a == 9 && b == 8 && c == 9 && d == 9)
+break;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
