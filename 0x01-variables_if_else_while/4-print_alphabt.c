754a7e1483e192ed5fd20a2845769e062760c306
maramilod <marammilod03@gmail.com>
2023-07-14 02:55:33 -0700

    betty

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+
 /**
-* main - entry point
-* putchar: output
-* Return: Always (0)
-*/
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char alpha; 
-for (alpha = 'a'; alpha <= 'z'; alpha++)
-{
-if (alpha != 'q' && alpha != 'e')putchar(alpha);
-}
-putchar('\n');
-return (0);
+    char alpha;
+
+    for (alpha = 'a'; alpha <= 'z'; alpha++)
+    {
+        if (alpha != 'q' && alpha != 'e')
+            putchar(alpha);
+    }
+    putchar('\n');
+
+    return (0);
 }
