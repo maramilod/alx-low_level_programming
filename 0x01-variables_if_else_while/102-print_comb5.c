34ec3b664d61a60c76512e67a64aac94efd2c474
maramilod <marammilod03@gmail.com>
2023-07-14 04:46:13 -0700

    eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,11 +7,13 @@
 int main(void)
 {
 int a, b, c, d;
+ 
 for (a = 0; a <= 9; a++)
 for (b = 0; b < 9; b++)
 for (c = 0; c <= 9; c++)
-for (d = b+1; d <= 9; d++)
+for (d = 0; d <= 9; d++)
 {
+  if (!(d >= c)){
 putchar(a + '0');
 putchar(b + '0');
 putchar(' ');
@@ -21,7 +23,7 @@ if ( a == 9 && b == 8 && c == 9 && d == 9)
 break;
 putchar(',');
 putchar(' ');
-}
+    }}
 putchar('\n');
 return (0);
 }
