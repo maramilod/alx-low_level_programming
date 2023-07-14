ef1449b3db9d02379fc53cbfcdd3c4f9281926a3
maramilod <marammilod03@gmail.com>
2023-07-14 05:11:50 -0700

    / || %

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * putchar: output
- * Return: Always (0)
- */
+* main - Entry point
+* putchar: output
+* Return: Always (0)
+*/
 int main(void)
 {
 int a, b;
- 
 for (a = 0; a <= 99; a++)
 for (b = a; b <= 99; b++)
 if (a != b)
@@ -15,9 +14,9 @@ if (a != b)
 putchar((a / 10) + 48);
 putchar((a % 10) + 48);
 putchar(' ');
-putchar((b /10) + '0');
-putchar((b %10) + '0');
-if ( a != 98 || b != 99)
+putchar((b / 10) + '0');
+putchar((b % 10) + '0');
+if (a != 98 || b != 99)
 {
 putchar(',');
 putchar(' ');
