330b3fe43b72959e76355e75f0ab634a1650a31d
maramilod <marammilod03@gmail.com>
2023-07-14 03:35:03 -0700

    break

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i<9; i++)
+for (i = 0; i < 9; i++)
 for (j = 0; j <= 9; j++)
-  if (!(i >= j))
+if (!(i >= j))
 {
 putchar(i + '0');
 putchar(j + '0');
-if(i == 8 && j == 9)
+if (i == 8 && j == 9)
 break;
 else
 putchar(',');
