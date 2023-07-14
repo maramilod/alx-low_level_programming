0453e310367142a0cf8b788836ae6e831975ee12
maramilod <marammilod03@gmail.com>
2023-07-14 02:58:24 -0700

    betty

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-    char alpha;
+char alpha;
 
-    for (alpha = 'a'; alpha <= 'z'; alpha++)
-    {
-        if (alpha != 'q' && alpha != 'e')
-            putchar(alpha);
-    }
-    putchar('\n');
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+ {
+  if (alpha != 'q' && alpha != 'e')
+   putchar(alpha);
+ }
+putchar('\n');
 
-    return (0);
+return (0);
 }
