cd8fdff2d555151607c4fbce7dd16f3e2094381a
maramilod <marammilod03@gmail.com>
2023-07-18 06:24:37 -0700

    blabla

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,14 @@
  *4-isalpha.c - Shows 1 if c is a letter, lowercase or uppercase
  *shows 0 otherwise
  *
+ *@c: the charcter in ACII code
+ *
  *Return: Returns 1 if c is a letter, lowercase or uppercase
  *Returns 0 otherwise
  */
 int _isalpha(int c)
 {
-if ((c >= 97 && c<= 122) || (c <= 90 && c >= 65))
+if ((c >= 97 && c <= 122) || (c <= 90 && c >= 65))
 {
 return (1);
 }
@@ -18,4 +20,3 @@ return (0);
 }
 _putchar('\n');
 }
-    
