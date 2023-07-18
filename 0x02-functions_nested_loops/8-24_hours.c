c32f38b557ce4aeb838024c4895b31a889d99d99
maramilod <marammilod03@gmail.com>
2023-07-18 11:30:57 -0700

    add

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,23 @@
 #include "main.h"
 
 /**
- * jack_baur - Prints the minutes of a day
- *
- * Return: no return
+ * jack_bauer - prints every minute of the day
  */
+
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int min, hr;
 
-	for (a = 0; a <= 2; a++)
+	for (hr = 0; hr <= 23; hr++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (min = 0; min <= 59; min++)
 		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					if (a >= 2 && b >= 4)
-						break;
-					_putchar(a + 48);
-					_putchar(b + 48);
-					_putchar(58);
-					_putchar(c + 48);
-					_putchar(d + 48);
-					_putchar('\n');
-				}
-			}
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
 		}
 	}
 }
