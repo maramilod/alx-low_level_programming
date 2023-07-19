17e17a778e11715d7764eb7a2c9824122bc97fca
maramilod <marammilod03@gmail.com>
2023-07-19 01:36:59 -0700

    add .

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/* more headers goes there */
+/**
+*  positive or negative - test the input if it is positive or
+*  negative or zero
+*  printf: OutPut
+*  @n: input
+*/
+/* betty style doc for function main goes there */
+void positive_or_negative(int n)
+{
+if (n == 0)
+printf("%d is zero\n", n);
+if (n > 0)
+printf("%d is positive\n", n);
+if (n < 0)
+printf("%d is negative\n", n);
+	/* your code goes there */
+}
