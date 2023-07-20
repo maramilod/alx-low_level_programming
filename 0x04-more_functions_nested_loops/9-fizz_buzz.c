e7517311a700a84df912cc14e731c0e014832d6f
maramilod <marammilod03@gmail.com>
2023-07-20 12:41:37 -0700

    add

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,14 +9,16 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-		if (i % 3 == 0)
-			printf(" Fizz");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
 		else if (i % 5 == 0)
-			printf(" Buzz");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf(" FizzBuzz");
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i == 100)
+			printf("Buzz");
 		else
-			printf(" %d", i);
+			printf("%d ", i);
 	printf("\n");
 	return (0);
 }
