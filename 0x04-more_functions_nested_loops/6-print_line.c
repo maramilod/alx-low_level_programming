eb9d8d210330541e9c38ecf6dbe0f6a386d54e2c
maramilod <marammilod03@gmail.com>
2023-07-20 09:00:35 -0700

    add

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_line -  function to print
+ *character _ should be printed
+ *
+ *@n: input
+ *
+ * _putchar: output
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+		_putchar('_');
+			_putchar('\n');
+}
