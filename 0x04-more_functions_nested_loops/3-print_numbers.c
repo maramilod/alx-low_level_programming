a4eee5482da9fdabd5913eacf0373d9908d70deb
maramilod <marammilod03@gmail.com>
2023-07-20 08:09:36 -0700

    add

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers - function print number
+ *
+ *
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9 ; i++)
+		_putchar(i + 48);
+	_putchar('\n');
+}
