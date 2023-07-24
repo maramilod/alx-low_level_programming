50550dcd4f97bd2778b227df3f574572b7849e2a
maramilod <marammilod03@gmail.com>
2023-07-24 09:42:25 -0700

    add

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcpy - function copies the string
+ *
+ * @dest: string pointir
+ * @src: string pointer
+ * Return:return pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != 0)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
