ccfbd93c066e177c209cc5f9a6e32eac1593b0ce
maramilod <marammilod03@gmail.com>
2023-07-24 11:12:26 -0700

    add

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,14 +8,13 @@
 
 int _atoi(char *s)
 {
-	unsigned int i = 0;
-	int number = 0;
+	unsigned int number = 0;
 	int gn = 1;
 
 	do {
-		if (s[i] == '-')
+		if (*s == '-')
 			gn *= -1;
-		else if (s[i] >= 48 && s[i] <= 57)
+		else if (*s >= 48 && *s <= 57)
 			number = (number * 10) + (*s - 48);
 		else if (number > 0)
 			break;
