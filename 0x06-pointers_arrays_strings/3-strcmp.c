2225f212a73eee5fe6facae3f0e7ed489f78232d
maramilod <marammilod03@gmail.com>
2023-07-27 12:25:33 -0700

    aa

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,8 +13,12 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+		{
 		com = (s1[i] - 48) - (s2[i] - 48);
-		break;
+		return (com);
+		}
+		i++;
 	}
-	return (com);
+	return (0);
 }
