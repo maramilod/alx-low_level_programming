80d04101553aa3a157d66ab63225225f468d9ae8
maramilod <marammilod03@gmail.com>
2023-08-02 09:38:48 -0700

    add

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,30 @@
 #include "main.h"
 /**
+ * int is_prime_number - function that returns the natural
+ * square root of a number
+ *
+ * @n: number
+ * Return: square
  */
 int is_prime_number(int n)
 {
+	if (n < 0)
+		n *= -1;
+	return (sqrtt(n, 1));
+}
+/**
+ * sqrtt - function
+ * @n: number
+ * @i: number -_-
+ * Return: result
+ */
 
+int sqrtt(int n, int i)
+{
+if (i * i < n)
+return (sqrtt(n, i + 1));
+		if (i * i > n)
+		return (1);
+		else
+		return (0);
+}
