3b8c499ad9c69a128260a9a11067ae0c8b1b4a2d
maramilod <marammilod03@gmail.com>
2023-08-02 10:43:25 -0700

    add

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * is_palindrome -  function that returns 1 if a string
+ * is a palindrome and 0 if not
+ * @s: pointer
+ * Return: result
+ */
+int is_palindrome(char *s)
+{
+	int last = length(s);
+
+	return (pal(s, 0, last - 1, last % 2));
+			}
+			/**
+			 * length - function
+			 * @s: pointer
+			 * Return: result
+			 */
+			int length(char *s)
+			{
+			int i = 0;
+
+			if (*s)
+			i += length(s + 1) + 1;
+			return (i);
+			}
+			/**
+			 * pal - function
+			 * @s: pointer
+			 * @b: beginning
+			 * @e: end
+			 * @m : odd or even
+			 */
+			int pal(char *s, int b, int e ,int m)
+{
+	if ((b == e && m  % 2 != 0) || (b == e + 1 && m % 2 == 0))
+		return (1);
+	if (s[b] != s[e])
+		return (0);
+	else
+		return (pal(s, b + 1, e - 1, m));
+				}
