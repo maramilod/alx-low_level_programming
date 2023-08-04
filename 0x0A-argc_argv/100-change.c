169e4d286adbbee51b06562db63a4e0b2a63b9dc
maramilod <marammilod03@gmail.com>
2023-08-04 07:38:29 -0700

    add

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,50 +4,37 @@
  * main - entry point
  * @argc: int
  * @argv: pointer
+ * Return: 0 or 1
  */
-
 int main(int argc, char *argv[])
 {
-	int cen, coins;
-	coins = 0;
+	int cen, coins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 	cen = atoi(argv[1]);
-
 	if (cen < 0)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 	while (cen > 0)
 	{
 		coins++;
 		if ((cen - 25) >= 0)
-		{
 			cen -= 25;
-			continue;
-		}
-		if ((cen - 10) >= 0)
-		{
-                        cen -= 10;
-                        continue;
-                }
-		if ((cen - 5) >= 0)
-		{
-                        cen -= 5;
-                        continue;
-                }
-		if ((cen - 2) >= 0)
-		{
-                        cen -= 2;
-                        continue;
-                }
-		cen--;
+		else if ((cen - 10) >= 0)
+			cen -= 10;
+		else if ((cen - 5) >= 0)
+			cen -= 5;
+		else if ((cen - 2) >= 0)
+			cen -= 2;
+		else
+			cen--;
 	}
-	printf("%d\n", coins);
-	return 0;
+		printf("%d\n", coins);
+		return (0);
 }
