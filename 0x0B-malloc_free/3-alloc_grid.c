55de8dcf572f8fb9090771514b2800ac517bcb1e
maramilod <marammilod03@gmail.com>
2023-08-07 04:37:33 -0700

    add

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * **alloc_grid - function
+ * @width: column
+ * @height: row
+ * Return: null or ar
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int j;
+	int **ar;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	ar = malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+		ar[i] = malloc(sizeof(int) * width);
+	if (!ar)
+		return (NULL);
+	i = 0;
+	while (i < height)
+	{
+		j = 0;
+		while (j < width)
+		{
+			ar[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return (ar);
+}
