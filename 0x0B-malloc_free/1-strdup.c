5db154bd3415ea368e6ce20fe72af347e47394c6
maramilod <marammilod03@gmail.com>
2023-08-07 02:33:22 -0700

    add

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,10 +9,12 @@ char *_strdup(char *str)
 	int j, i = 0;
 	char *ar;
 
+	if (str == NULL)
+		return (NULL);
 	while (str[i])
 		i++;
 	ar = malloc(sizeof(char) * i);
-	if (!ar || !str || str == NULL)
+	if (!ar || !str)
 		return (NULL);
 	for (j = 0; j <= i; j++)
 		ar[j] = str[j];
