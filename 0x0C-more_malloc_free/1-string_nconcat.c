e1afb1596970c37171fae3a90e5d9ec002e3820c
maramilod <marammilod03@gmail.com>
2023-08-10 06:31:08 -0700

    add

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,7 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i <= n2; i++)
 		s3[i] = s1[i];
 	i--;
-	for (j = 0; j <= n; j++)
+	for (j = 0; j < n; j++)
 	{
 		s3[i] = s2[j];
 		i++;
