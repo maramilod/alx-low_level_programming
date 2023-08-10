fb10af21db56d07a171ddf53ded3d953c3a2f220
maramilod <marammilod03@gmail.com>
2023-08-10 09:23:01 -0700

    add

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 	if (max < min)
 		return (NULL);
 	n = max - min;
-	p = malloc(sizeof(int) * (n + 1));
+	p = malloc(sizeof(int) * (n + 1) + 1);
 	if (p == NULL)
 		return (NULL);
 	while (i <= n)
