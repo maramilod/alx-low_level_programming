ef17e14dd22836816673dab6d4d9dfdaf8f6ee0d
maramilod <marammilod03@gmail.com>
2023-08-14 04:54:06 -0700

    add

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - idk
+ * @name: dog's name :)
+ * @age: number
+ * @owner: name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
