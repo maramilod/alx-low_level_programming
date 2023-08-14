995e090f41b5d95b408bf908a2041fa7d0a42ce8
maramilod <marammilod03@gmail.com>
2023-08-14 11:08:08 -0700

    add

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @age: number
  * @owner: name
  */
-struct dog
+typof struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
