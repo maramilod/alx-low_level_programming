0c3bb362e75fc74a04be7b00b65235e567594a68
maramilod <marammilod03@gmail.com>
2023-08-15 23:46:58 -0700

    add

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function
+ * @array: array
+ * @size: size of array
+ *@action: pointer of action
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
