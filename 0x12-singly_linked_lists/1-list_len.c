2f79e99dfc6813f31d477a9209ac541d0e7b6207
maramilod <marammilod03@gmail.com>
2023-08-24 09:13:36 -0700

    okokokok

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - function that return the number
+ * of elements in a linked list
+ *
+ * @h: header
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
