1dcd3a8969ece1c261ce10e169a950a79aaef5b9
maramilod <marammilod03@gmail.com>
2023-08-24 09:04:28 -0700

    okokokok

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - function that prints all elements
+ * of a list
+ *
+ * @h: header
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
