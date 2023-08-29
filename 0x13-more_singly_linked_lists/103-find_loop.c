1d9c8222311d2d790ba854e1a6fe7e97522de946
maramilod <marammilod03@gmail.com>
2023-08-29 13:21:55 -0700

    add

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * find_listint_loop - function
+ *
+ * @head: lala
+ * Return: address
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *a = NULL;
+	int i = 0;
+
+	while (head)
+	{
+		head = head->next;
+	}
+	return (a);
+}
