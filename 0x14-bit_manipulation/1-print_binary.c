8d8176b3acf17bfb3a2e9b6aa2ae88dcbb2ebe79
maramilod <marammilod03@gmail.com>
2023-08-31 08:41:53 -0700

    add

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,15 +6,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int num;
 	int i, cou = 0;
 
-	if (!n)
-		return;
 	for (i = 63; i >= 0; i--)
 	{
-		num = n >> i;
-		if (num & 1)
+		if (n >> i & 1)
 		{
 			_putchar('1');
 			cou = 1;
