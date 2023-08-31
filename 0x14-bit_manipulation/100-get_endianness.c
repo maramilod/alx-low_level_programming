ffa18270877279a350cc0fcd5daacdf2cde4dd7d
maramilod <marammilod03@gmail.com>
2023-08-31 11:58:02 -0700

    add

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - function
+ * Return: 0 for larg 1 for small
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c = (char *) & i;
+
+	return (*c);
+}
