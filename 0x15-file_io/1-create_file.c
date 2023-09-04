96d8fffd7b8b6ba8013cff0365b0f7b0ba54c870
maramilod <marammilod03@gmail.com>
2023-09-04 12:10:05 -0700

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int f;
 
 	if (!filename)
-		return (0);
+		return (-1);
 	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (f == -1)
 		return (-1);
