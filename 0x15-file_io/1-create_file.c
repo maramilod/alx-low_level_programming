cc4bcd2a1e1e91df52743ce78542822e0b71fcb6
maramilod <marammilod03@gmail.com>
2023-09-04 12:17:19 -0700

    add

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,9 @@ int create_file(const char *filename, char *text_content)
 	if (f == -1)
 		return (-1);
 
-	while (*text_content++)
-		len++;
+	if (text_content)
+		while (*text_content++)
+			len++;
 
 	if (len)
 		byte = write(f, text_content, len);
