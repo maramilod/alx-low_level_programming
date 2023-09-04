fab43fb8a978bb923bcbfc1496ae1f12ee67baa5
maramilod <marammilod03@gmail.com>
2023-09-04 12:03:37 -0700

    add

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - function
+ * @filename: is the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return:1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t byte = 0, len = 0;
+	int f;
+
+	if (!filename)
+		return (0);
+	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (f == -1)
+		return (-1);
+
+	while (*text_content++)
+		len++;
+
+	if (len)
+		byte = write(f, text_content, len);
+	close(f);
+
+	return (byte == len ? 1 : -1);
+}
